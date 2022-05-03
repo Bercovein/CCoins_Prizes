424bfdf8ddcedcd790136601ccf3998b2327b938
bercovein <bercovsky.nm@gmail.com>
2022-05-02 22:12:09 -0300

    first commit

---
+++ src/test/java/com/ccoins/Prizes/PrizesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccoins.Prizes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrizesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
